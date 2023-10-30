9f80aa028004c37a41ab288ee1210906f24b3251
johnSamilin <temp.kroogi@gmail.com>
2023-10-30 16:11:26 +0300

    Added database

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mail-ru-im/bot-golang v0.0.0-20230426110355-ca7c8eadc73d // indirect
 	github.com/mailru/easyjson v0.0.0-20190626092158-b2ccc519800e // indirect
+	github.com/mattn/go-sqlite3 v1.14.17 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	golang.org/x/sys v0.0.0-20211013075003-97ac67df715c // indirect
 )
--- main.go
+++ main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"os"
+
+	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/joho/godotenv"
 	botgolang "github.com/mail-ru-im/bot-golang"
 )
+
+type userModel struct {
+	sn         string
+	first_name string
+	last_name  string
+}
+
+var SQL_SELECT_USER = "SELECT ыт, first_name, last_name FROM users WHERE sn=?"
+var SQL_CREATE_USER = "INSERT INTO users (first_name, last_name, sn) VALUES(?, ?, ?)"
 
 func main() {
 	envErr := godotenv.Load(".env")
@@ -15,6 +27,18 @@ func main() {
 		log.Println("You have to create .env file")
 		return
 	}
+
+	db, dbErr := sql.Open("sqlite3", "./standsbot.sqlite")
+	if dbErr != nil {
+		log.Panicln("Error in DB", dbErr)
+		return
+	}
+	db.SetMaxOpenConns(1)
+	userSelectStmt, _ := db.Prepare(SQL_SELECT_USER)
+	defer userSelectStmt.Close()
+	userInsertStmt, _ := db.Prepare(SQL_CREATE_USER)
+	defer userInsertStmt.Close()
+	defer db.Close()
 
 	var TOKEN = os.Getenv("TOKEN")
 	var API_URL = os.Getenv("API_URL")
@@ -26,10 +50,17 @@ func main() {
 	}
 
 	ctx, finish := context.WithCancel(context.Background())
+	defer finish()
 	updates := bot.GetUpdatesChannel(ctx)
 	for update := range updates {
-		log.Println(update)
+		if update.Type == "newMessage" {
+			sn := update.Payload.BaseEventPayload.From.ID
+			users, selectErr := userSelectStmt.Exec(sn)
+			if selectErr != nil {
+				log.Println(selectErr)
+			}
+			log.Println(users)
+		}
 	}
 
-	finish()
 }
